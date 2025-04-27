af469db877c46e3bd11c04a512ecf186c89f80aa
ronish maharjan <ronishmaharjan01@gmail.com>
2025-04-27 21:05:04 +0545

    initial commit

---
+++ main.js
@@ -0,0 +1,85 @@
+const fs = require("fs/promises");
+const path = require("path");
+
+// Function to create the directory
+const createDirectory = async (dirPath, fileObject) => {
+  let fileInfoArray = fileObject;
+  for (let obj of fileInfoArray) {
+    const { folder } = obj;
+    try {
+      const fullPath = path.join(dirPath, folder);
+      await fs.mkdir(fullPath, { recursive: true }); // Create directory if it doesn't exist
+    } catch (e) {
+      if (e.code === "ENOENT") {
+        console.log("Directory not found:", e);
+      } else {
+        console.log("Error creating directory:", e);
+      }
+    }
+  }
+
+  // Call moveFile once directories are created
+  await moveFile(fileObject);
+};
+
+// Function to move file in the right folder
+const moveFile = async (fileObject) => {
+  for (let obj of fileObject) {
+    const { oldPath, newPath } = obj;
+    await fs.rename(oldPath, newPath); // Move file
+  }
+};
+
+// Get clean data or filter clean data of files
+const getAll = async (folderPath) => {
+  const cleanData = [];
+  const folderAllData = await fs.readdir(folderPath);
+  const extensions = [
+    { text: ["json", "txt", "csv"] },
+    { music: ["mp3", "avi"] },
+    { movies: ["mp4"] },
+  ];
+
+  // To loop through each folder and file
+  try {
+    for (let data of folderAllData) {
+      let oldPath = null;
+      let newPath = null;
+      let folder = null;
+      const dataFullPath = path.join(folderPath, data);
+      const dataInfo = await fs.stat(dataFullPath);
+      if (dataInfo.isFile()) {
+        oldPath = dataFullPath;
+        const fileExtension = path.extname(dataFullPath);
+        const cleanFileExtension = fileExtension.replace(".", ""); // Remove leading dot
+        for (let obj of extensions) {
+          for (let key in obj) {
+            if (obj[key].includes(cleanFileExtension)) {
+              newPath = path.join(folderPath, `${key}/${data}`);
+              folder = key;
+              break;
+            }
+          }
+          if (newPath) break;
+        }
+        if (!newPath) {
+          newPath = path.join(folderPath, `others/${data}`);
+          folder = "others";
+        }
+        cleanData.push({ oldPath, newPath, folder });
+      }
+    }
+    return cleanData;
+  } catch (e) {
+    console.log(`There occurred an error: ${e}`);
+  }
+};
+
+// Main function
+const main = async () => {
+  const __currentDirectory = __dirname; // In CommonJS, __dirname gives the current directory of the script
+  const data = await getAll(__currentDirectory);
+  await createDirectory(__currentDirectory, data);
+};
+
+main();
---
+++ readme.md
@@ -0,0 +1,27 @@
+# File Organizer By Ronish
+
+This is a Node.js script that organizes files in a directory based on their extensions. The script categorizes files into predefined folders such as `json`, `txt`, `csv`, `music`, `movies`, and `others` based on file type.
+
+## Features
+
+- Automatically organizes files into categorized folders based on their extensions.
+- Supports custom extensions for different categories.
+- Creates folders dynamically if they don't exist.
+- Moves files to the appropriate folder after organization.
+- Add Own Extension and Folders.
+
+## Setup
+
+### 1. Download NodeJs
+
+### 2. Clone the repository:
+
+`git clone https://github.com/maharjanronish/file-organizer`
+
+### 3. Copy the main.js to the folder you want to organize
+
+### 4. Open terminal in that folder
+
+### 5. Run the given command::
+
+`node main.js`
